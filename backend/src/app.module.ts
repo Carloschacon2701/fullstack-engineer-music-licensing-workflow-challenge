383a66bedf179404d24216164113430bff73f68d
Daniel Chacon <carlosdchaconmendez@gmail.com>
2025-11-13 18:51:59 -0400

    Feat: Update app module to include Movies, Tracks, Scenes, Songs, and Licenses modules for improved functionality.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { TracksModule } from './tracks/tracks.module';
+import { ScenesModule } from './scenes/scenes.module';
+import { SongsModule } from './songs/songs.module';
+import { LicensesModule } from './licenses/licenses.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MoviesModule,
+    TracksModule,
+    ScenesModule,
+    SongsModule,
+    LicensesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
