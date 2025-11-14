888f4d06d9335ecc52818582f15b18c4749db3bd
Daniel Chacon <carlosdchaconmendez@gmail.com>
2025-11-14 08:03:40 -0400

    Feat: Implement TypeORM configuration and add initial database migration for movie-related entities

--- backend/src/config/typeorm.config.ts
+++ backend/src/config/typeorm.config.ts
@@ -1,9 +1,35 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { dataSource } from '@/db/datasource';
+import { DataSourceOptions } from 'typeorm';
+import { LicenseStatusHistory } from '@/modules/licenses/entities/license-status-history.entity';
+import { Status } from '@/modules/licenses/entities/status.entity';
+import { Movie } from '@/modules/movies/entities/movie.entity';
+import { Scene } from '@/modules/scenes/entities/scene.entity';
+import { Song } from '@/modules/songs/entities/song.entity';
+import { Track } from '@/modules/tracks/entities/track.entity';
+import { License } from '@/modules/licenses/entities/license.entity';
 
 export const TypeOrmConfig = TypeOrmModule.forRootAsync({
   imports: [ConfigModule],
   inject: [ConfigService],
-  dataSourceFactory: async () => dataSource,
+  useFactory: (configService: ConfigService): DataSourceOptions => ({
+    type: 'mysql',
+    host: configService.get('DB_HOST'),
+    port: +configService.get('DB_PORT'),
+    username: configService.get('DB_USER'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    synchronize: false,
+    migrationsRun: true,
+    migrations: [__dirname + '/../db/migrations/*.ts'],
+    entities: [
+      Movie,
+      Scene,
+      Song,
+      Track,
+      License,
+      Status,
+      LicenseStatusHistory,
+    ],
+  }),
 });
--- backend/src/db/datasource.ts
+++ backend/src/db/datasource.ts
@@ -4,12 +4,6 @@ import { ConfigService } from '@nestjs/config';
 
 const configService = new ConfigService();
 
-console.log(configService.get('DB_HOST'));
-console.log(configService.get('DB_PORT'));
-console.log(configService.get('DB_USERNAME'));
-console.log(configService.get('DB_PASSWORD'));
-console.log(configService.get('DB_NAME'));
-
 export const dataSource = new DataSource({
   type: 'mysql',
   host: configService.get('DB_HOST'),
@@ -19,5 +13,5 @@ export const dataSource = new DataSource({
   database: configService.get('DB_NAME'),
   entities: [__dirname + '/../**/*.entity.ts'],
   migrationsRun: true,
-  migrations: [__dirname + '/../migrations/*.ts'],
+  migrations: [__dirname + '/migrations/*.ts'],
 });
